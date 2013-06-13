4cfe9144b1876058ae53c645f0533faab859c3be
p.borukova@gmail.com <p.borukova@gmail.com@6d225db7-3968-b294-be7e-c09b120d6025>
2013-06-13 16:21:54 +0000

    Adding hints for actions after game is over / won

--- trunk/XonixGame/scripts/GameRules.js
+++ trunk/XonixGame/scripts/GameRules.js
@@ -3,53 +3,48 @@
         this.player = player;
         this.enemy = enemy;
         this.playfield = playfield;
+        this.lives = 5;
     },
-    validatePosition: function (cell) {
-        // TODO if player can not move do nothing
-    },
-    hasCollisions: function() {
-        for (var i = 0; i < enemies.length; i++) {
-            if (enemies[i].doesEnemyCollide(i)) {
-                return true;
-            }
+
+    hasCollisions: function () {
+        var collision = false;
+
+        if (this.player.position.leftPosition == this.enemy.position.leftPosition &&
+            this.player.position.rightPosition == this.enemy.position.rightPosition) {
+            collision = true;
         }
 
-        return false;
+        return collision;
     },
 
-    doesEnemyCollide : function (index) {
-        var enemy = enemies[index];
-
-        for (var i = 0; i < pathSoFar.length; i++) {
-            if (enemy.cell.equals(pathSoFar[i].cell)) {
-                return true;
-            }
-        }
-
-        return false;
+    hasLost: function () {
+        return this.lives == 0;
     },
-    isBorderReached : function (playerPositionCell, playfield) {
-        for (var i = 0; i < playfield.array.length; i++) {
-            for (var j = 0; j < playfield.array[i].length; j++) {
-                if (playfield.array[i][j].position.leftPosition === playerPositionCell.position.leftPosition &&
-                        playfield.array[i][j].position.topPosition === playerPositionCell.position.topPosition) {
-                    return true;
-                }
-            }
-        }
 
-        return false;
-    },
-    isPlayerStepOnHisPath: function (playerPositionCell, pathSoFar) {
-        for (var i = 0; i <pathSoFar.length; i++) {
-            if (playerPositionCell.position.leftPosition === pathSoFar[i].position.leftPosition &&
-                playerPositionCell.position.topPosition === pathSoFar[i].position.topPosition) {
-                return true;
-            }
-        }
+    //isBorderReached: function (playerPositionCell, playfield) {
+    //    for (var i = 0; i < playfield.array.length; i++) {
+    //        for (var j = 0; j < playfield.array[i].length; j++) {
+    //            if (playfield.array[i][j].position.leftPosition === playerPositionCell.position.leftPosition &&
+    //                    playfield.array[i][j].position.topPosition === playerPositionCell.position.topPosition) {
+    //                return true;
+    //            }
+    //        }
+    //    }
 
-        return false;
-    },
+    //    return false;
+    //},
+
+    //isPlayerStepOnHisPath: function (playerPositionCell, pathSoFar) {
+    //    for (var i = 0; i < pathSoFar.length; i++) {
+    //        if (playerPositionCell.position.leftPosition === pathSoFar[i].position.leftPosition &&
+    //            playerPositionCell.position.topPosition === pathSoFar[i].position.topPosition) {
+    //            return true;
+    //        }
+    //    }
+
+    //    return false;
+    //},
+
     hasWon: function (playfield) {
         if (playfield.calculatePoints() > 1700) {
             return true;
