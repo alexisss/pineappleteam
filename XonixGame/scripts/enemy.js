919918b3067903df21989cda6dc6ac3eebb33e7b
p.borukova <p.borukova@gmail.com>
2013-06-13 17:54:29 +0000

    Fixing enemy moving (only right - in the method moveTopLeft)

--- XonixGame/scripts/enemy.js
+++ XonixGame/scripts/enemy.js
@@ -3,21 +3,27 @@
         this.position = position;
         this.color = color;
         this.radius = 10;
-        this.x = 1;
-        this.y = 1;
+        this.x = 14;
+        this.y = 10;
     },
 
-    moveTopRight: function() {
-
-        if (!playfield.isSown(new CellTopLeft(this.position.leftPossition + this.x, this.position.topPossition + this.y))) {
+    moveTopRight: function () {
+        var newLeft = this.position.leftPossition + this.x;
+        var newTop = this.position.topPossition + this.y;
+        var colIndex = parseInt((newLeft) / 14);
+        var rowIndex = parseInt((newTop) / 10);
+        if (!playfield.isSown(rowIndex, colIndex)) {
             x = x;
             y = y;
         }
-
         changePosition();
     },
 
-    moveBottomLeft: function(){
+    moveBottomLeft: function () {
+        var newLeft = this.position.leftPossition + this.x;
+        var newTop = this.position.topPossition + this.y;
+        var colIndex = parseInt((newLeft) / 14);
+        var rowIndex = parseInt((newTop) / 10);
         if (!playfield.isSown(new CellTopLeft(this.position.leftPossition - this.x, this.position.topPossition - this.y))) {
             х = х * (-1);
             y = y * (-1);
@@ -26,16 +32,31 @@
         changePosition();
     },
 
-    moveTopLeft: function() {
-        if (!playfield.isSown(new CellTopLeft(this.position.leftPossition - this.x, this.position.topPossition + this.y))) {
-            х = х * (-1);
+    moveTopLeft: function (playfield) {
+        var newLeft = this.position.leftPossition + this.x;
+        var newTop = this.position.topPossition;
+        var colIndex = parseInt((newLeft) / 14);
+        var rowIndex = parseInt((newTop) / 10);
+        if (this.position.leftPosition > 0 && !playfield.isSown(rowIndex, colIndex) ) {
+            this.position.leftPosition += this.x / 2;
         }
+        //var newLeft = this.position.leftPossition + this.x;
+        //var newTop = this.position.topPossition + this.y;
+        //var colIndex = parseInt((newLeft) / 14);
+        //var rowIndex = parseInt((newTop) / 10);
+        //if (!playfield.isSown(new CellTopLeft(this.position.leftPossition - this.x, this.position.topPossition + this.y))) {
+        //    х = х * (-1);
+        //}
 
-        changePosition();
+        //changePosition();
     },
 
-    moveBottomRight: function(){
-        if (!playfield.isSown(new CellTopLeft(this.position.leftPossition + this.x, this.position.topPossition - this.y))) {
+    moveBottomRight: function () {
+        var newLeft = this.position.leftPossition + this.x;
+        var newTop = this.position.topPossition - this.y;
+        var colIndex = parseInt((newLeft) / 14);
+        var rowIndex = parseInt((newTop) / 10);
+        if (!playfield.isSown(rowIndex, colIndex)) {
             y = y * (-1);
         }
 
@@ -43,24 +64,25 @@
         
     },
 
-    move: function () {
-        var direction = getRandomInt(1, 4);
-        switch (direction) {
-            case 1:
-                this.moveTopRight();
-                break;
-            case 2:
-                this.moveBottomLeft();
-                break;
-            case 3:
-                this.moveTopRight();
-                break;
-            case 4:
-                this.moveBottomLeft();
-                break;
-            default:
-                break;
-        }
+    move: function (playfield) {
+        //var direction = getRandomInt(1, 4);
+        //switch (direction) {
+        //    case 1:
+        //        this.moveTopRight();
+        //        break;
+        //    case 2:
+        //        this.moveBottomLeft();
+        //        break;
+        //    case 3:
+        //        this.moveTopRight();
+        //        break;
+        //    case 4:
+        //        this.moveBottomLeft();
+        //        break;
+        //    default:
+        //        break;
+        //}
+        this.moveTopLeft(playfield);
     }
 });
 
