3b85d9ea02653aea8d592f41f31e2ac065022c07
p.borukova@gmail.com <p.borukova@gmail.com@6d225db7-3968-b294-be7e-c09b120d6025>
2013-06-13 14:43:57 +0000

    Drawing the enemy and the player and making the player move

--- trunk/XonixGame/scripts/xonix.js
+++ trunk/XonixGame/scripts/xonix.js
@@ -1,1 +1,54 @@
-﻿
+﻿var playfield = new Playfield();
+
+var cellWidth = 14;
+var cellHeight = 10;
+
+var ct1 = new CellTopLeft(0, 0);
+var ct2 = new CellTopLeft(14, 10);
+var ct3 = new CellTopLeft(28, 10);
+var ct10 = new CellTopLeft(56, 10);
+var c1 = new Cell(ct1, "red");
+var c2 = new Cell(ct2, "red");
+var c3 = new Cell(ct3, "purple");
+var c10 = new Cell(ct10, "green");
+var arr = new Array(c1, c2, c3, c10);
+playfield.sow(arr);
+
+var gameDraw = new GameDraw();
+gameDraw.drawField(playfield);
+var gameRules = new GameRules();
+var ct4 = new CellTopLeft(28, 10);
+var playerCell = new Cell(ct4, "red");
+
+var playerPosition = new CellTopLeft(50, 50);
+var player = new Player(playerPosition);
+
+var enemyPosition = new CellTopLeft(150, 150);
+var enemy = new Enemy(enemyPosition);
+
+setInterval(function () {
+    gameDraw.clearAll();
+    gameDraw.draw(player, playfield, enemy)
+}, 50);
+
+
+
+
+$("body").keydown(function (e) {
+    if (e.keyCode == 37) { // left   
+        player.moveLeft(cellWidth);
+
+    }
+    else if (e.keyCode == 38) {
+        player.moveUp(cellHeight);
+
+    }
+    else if (e.keyCode == 39) { // right
+
+        player.moveRight(cellWidth);
+
+    }
+    else if (e.keyCode == 40) { // down
+        player.moveDown(cellHeight);
+    }
+});
