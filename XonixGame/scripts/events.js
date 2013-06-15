23e4ca26bf594bb99b38111b28bb406528424a09
p.borukova <p.borukova@gmail.com>
2013-06-15 16:44:33 +0000

    Adding levels - easy, medium and hard (but looking ugly)

---
+++ XonixGame/scripts/events.js
@@ -0,0 +1,48 @@
+﻿$("body").keydown(function (e) {
+
+    if (e.keyCode == 37) { // left   
+        player.moveLeft(cellWidth);
+        seedVisitedCell(player);
+        gameRules.checkForCollisions();
+    }
+    else if (e.keyCode == 38) {
+        player.moveUp(cellHeight);
+        seedVisitedCell(player);
+        gameRules.checkForCollisions();
+    }
+    else if (e.keyCode == 39) { // right
+
+        player.moveRight(cellWidth, gameDraw.canvas.width);
+        seedVisitedCell(player);
+        gameRules.checkForCollisions();
+    }
+    else if (e.keyCode == 40) { // down
+        player.moveDown(cellHeight, gameDraw.canvas.height);
+        seedVisitedCell(player);
+        gameRules.checkForCollisions();
+    }
+});
+
+
+$("#reset-button").click(function () {
+    reset();
+});
+
+$(".difficulty").click(function (e) {
+    if (e.currentTarget.defaultValue == "Easy") {
+        enemies = [];
+        enemies.push(enemy);
+    }
+    if (e.currentTarget.defaultValue == "Medium") {
+        enemies = [];
+        enemies.push(enemy);
+        enemies.push(secondEnemy);
+    }
+    if (e.currentTarget.defaultValue == "Hard") {
+        enemies = [];
+        enemies.push(enemy);
+        enemies.push(secondEnemy);
+        enemies.push(thirdEnemy);
+    }
+});
+
