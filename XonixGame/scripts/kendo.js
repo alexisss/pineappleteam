45a0149966e5b1c5aaf241eaa7d4a8d7ee092ab8
p.borukova <p.borukova@gmail.com>
2013-06-15 13:17:38 +0000

    Adding some css (now it looks cool) and moving javascript in different files

---
+++ XonixGame/scripts/kendo.js
@@ -0,0 +1,28 @@
+﻿$("#color-chooser").kendoColorPalette({
+    palette: ["basic"],
+    tileSize: 30,
+    value: "#99d9ea",
+    change: function () {
+        var colorId = this.value().substring(1);
+        $('#playfield').css('background-color', '#' + colorId);
+    }
+});
+
+
+$(document).ready(function () {
+    $("#players").kendoComboBox({
+        filter: "startswith",
+        dataTextField: "ContactName",
+        dataValueField: "CustomerID",
+        template: '<img src="styles/img/${data.ContactName}.jpg" alt=\"${data.ContactName}\" height="150" />' +
+                '<h3>${ data.ContactName }</h3>' +
+                '<p>${ data.CompanyName }</p>',
+        dataSource: [{ "ContactName": "GrandPa", "CompanyName": "Pineapple Team" }, { "ContactName": "GrandMa", "CompanyName": "Pineapple Team" }]
+    });
+
+    var combobox = $("#players").data("kendoComboBox");
+    combobox.bind("change", function (e) {
+        changePlayerType(e, this);
+    });
+    combobox.select(0);
+});
