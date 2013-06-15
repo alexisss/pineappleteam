838f1c6a6f82c7702a3a9438e6915b32934d8a42
p.borukova@gmail.com <p.borukova@gmail.com@6d225db7-3968-b294-be7e-c09b120d6025>
2013-06-15 13:17:38 +0000

    Adding some css (now it looks cool) and moving javascript in different files

---
+++ trunk/XonixGame/scripts/facebook.js
@@ -0,0 +1,15 @@
+﻿(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/bg_BG/all.js#xfbml=1&appId=470404223053971";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+(function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/bg_BG/all.js#xfbml=1&appId=470404223053971";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
