94a9dbad7645fb50f5e69ab3099307ba7ed2d969
naturalna89 <naturalna89@gmail.com>
2013-06-15 10:58:17 +0000

    Make border and change player movements

--- XonixGame/scripts/player.js
+++ XonixGame/scripts/player.js
@@ -7,25 +7,25 @@ var Player = Class.create({
     },
 
     moveUp: function (distance) {
-        if (this.position.topPosition > 0) {
+        if (this.position.topPosition > 60) {
             this.position.topPosition -= distance;
         }
     },
 
     moveDown: function (distance, maxHeight) {
-        if (this.position.topPosition < maxHeight) { //this must be constant and not 100 but canvas height
+        if (this.position.topPosition < maxHeight - 10) { //this must be constant and not 100 but canvas height
             this.position.topPosition += distance;
         }
     },
 
     moveLeft: function (distance) {
-        if (this.position.leftPosition > 0) {
+        if (this.position.leftPosition > 28) {
             this.position.leftPosition -= distance;
         }
     },
 
     moveRight: function (distance, maxWidth) {
-        if (this.position.leftPosition < maxWidth) { //must be something like canvas width
+        if (this.position.leftPosition < maxWidth - 42) { //must be something like canvas width
             this.position.leftPosition += distance;
         }
     }
