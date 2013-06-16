09ed3f0480da59ba20f31336754e61420e3d4512
alexandra.d.atanasova <alexandra.d.atanasova@gmail.com>
2013-06-16 20:36:12 +0000

    CellTopLeftTest added

---
+++ XonixGame/scripts/testCellTopLeft.js
@@ -0,0 +1,8 @@
+﻿module("CellTopLeft.init");
+
+test("When cellTopLeft is initialized, should set the correct values", function () {
+    var cell = new CellTopLeft(14, 0);
+
+    equal(cell.topPosition, 0, "Cell top position is set");
+    equal(cell.leftPosition, 14, "Cell left position is set");
+});
