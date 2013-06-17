09d1b5cf11baaf7ec932a3c4b044006515c25ac4
Rumyana.Bankina <Rumyana.Bankina@gmail.com>
2013-06-17 09:15:54 +0000

    one more time

---
+++ XonixGame/scripts/testPlayfield.js
@@ -0,0 +1,16 @@
+﻿module("PlayfieldModule.init");
+
+test("Test if the cell is positioned correctly", function () {
+    var position = new CellTopLeft(100, 20);
+    var color = "red";
+
+    var cell = new PlayfieldModule.Cell(position, color);
+
+    equal(cell.position, position, "Cell positioning is correct");
+});
+
+test("Test if the playfiel is create with the correct dimentions", function () {
+    var playfield = new PlayfieldModule.Playfield(700, 400);
+
+    ok(playfield.canvasWidth == 700 && playfield.canvasHeight == 400, "Canvas dimentions are correct");
+});
